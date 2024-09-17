a6ffeb09d5b46def11f5b685539368db7c3ecc93
Matthew Bartels <matthewbartels@Macintosh.attlocal.net>
2024-09-17 18:32:44 -0400

    updated code for week 11 coding assignment

--- src/main/java/projects/service/ProjectsService.java
+++ src/main/java/projects/service/ProjectsService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import projects.dao.ProjectsDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 public class ProjectsService {
 	private ProjectsDao projectDao = new ProjectsDao();
@@ -20,6 +21,19 @@ public Project fetchProjectById(Integer projectId) {
 	return projectDao.fetchProjectById(projectId)
 			.orElseThrow(() -> new NoSuchElementException(
 			"Project with project ID-" + projectId + " does not exist."));
+}
+
+public void deleteProject(Integer projectId) {
+	if(!projectDao.deleteProject(projectId)) {
+		throw new DbException("Project with ID=" + projectId + " does not exist.");
+	}
+}
+
+public void modifyProjectDetails(Project project) {
+	if(!projectDao.modifyProjectDetails(project)) {
+		throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+	}
+	
 }
 
 }
